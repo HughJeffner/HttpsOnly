8b6e44417018aa6320e459150d89397b719f7aac
Jeff Lingis <hughjeffner@users.noreply.github.com>
2014-06-13 13:49:13 +0000

    Initial project import

---
+++ HttpsOnly/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("HttpsOnly")> 
+<Assembly: AssemblyDescription("A configurable HttpModule that forces a website to use https")> 
+<Assembly: AssemblyCompany("ICS")> 
+<Assembly: AssemblyProduct("HttpsOnly")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3668ea8e-aed8-4ee0-a613-a025ab7c2f14")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
